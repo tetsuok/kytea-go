0965388aa370331f41e102fe9a569ead2fce00da
Tetsuo Kiso <tetsuokiso9@gmail.com>
2014-09-24 01:04:03 +0900

    Fix travis CI script: install gyp

--- install_kytea.sh
+++ install_kytea.sh
@@ -5,6 +5,7 @@ git clone https://github.com/tetsuok/kytea libkytea
 cd libkytea
 . ci_config.sh
 bash ./setup_gtest.sh
+bash ./gyp/install_gyp.sh
 bash ./build_with_gyp.sh Release make
 if [[ "$TARGET_OS" = "Darwin" ]]; then
   cp out_mac/Release/libkytea.a ..
