52546c096832d4c3efaf0a921fa34f3192df0797
Tetsuo Kiso <tetsuokiso9@gmail.com>
2014-09-25 20:50:23 +0900

    Fix the installation of KyTea's library
    When setting up the building environment on Travis CI, install the library
    and headers under /usr/local.  Include /usr/local/lib in LD_LIBRARY_PATH
    just in case the path is not included in the environment variable.  The previous
    approach requires users to set up the environment varibles related to cgo.
    That is not easy if they are not familiar with cgo.  This change avoids
    setting the variables as long as they have correctly installed KyTea library under
    the system in which a compler finds the headers, and a dynamic linker finds
    the library.

--- install_kytea.sh
+++ install_kytea.sh
@@ -3,6 +3,7 @@ set -o errexit
 set -o nounset
 
 PWD=`pwd`
+PREFIX=/usr/local
 
 git clone https://github.com/tetsuok/kytea libkytea
 cd libkytea
@@ -10,12 +11,26 @@ cd libkytea
 bash ./setup_gtest.sh
 bash ./gyp/install_gyp.sh
 bash ./build_with_gyp.sh Release make
+
+# Copying a shared library
+SHARED_EXT=so
 if [[ "$TARGET_OS" = "Darwin" ]]; then
-  cp out_mac/Release/libkytea.a ..
-else
-  cp out_unix/Release/obj.target/gyp/libkytea.a ..
+  SHARED_EXT=dylib
 fi
-cp -fr src/include/kytea .
+SHARED_LIB=libkytea.$SHARED_EXT
+
+SHARED_PATH=out_unix/Release/lib.target
+if [[ "$TARGET_OS" = "Darwin" ]]; then
+  echo "OS X is not supported yet"
+  exit 1
+fi
+sudo cp $SHARED_PATH/$SHARED_LIB $PREFIX/lib
+
+# Copying headers
+if ! [[ -e $PREFIX/include ]]; then
+  sudo mkdir -p $PREFIX/include
+fi
+sudo cp -fr src/include/kytea $PREFIX/include
 
 ln -sf $PWD/data/model.bin ../sample
 cd ..
--- smoketest.sh
+++ smoketest.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -x
-PWD=`pwd`
-export CGO_LDFLAGS="-L$PWD"
-export CGO_CFLAGS="-I$PWD"
 cd sample
 go build
 ./sample
